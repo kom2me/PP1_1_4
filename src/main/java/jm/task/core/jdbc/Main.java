7282f9b63ced0897b4c09b7324d72176f827600c
komXXme <seoprokom@gmail.com>
2022-03-18 03:16:16 +0400

    Revert "Finish programm commit"
    This reverts commit 6cdae3ab35b8c9116d0e7f8965ca91608b6b1795.

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,31 +1,7 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.service.UserService;
-import jm.task.core.jdbc.service.UserServiceImpl;
-import jm.task.core.jdbc.util.Util;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
-
 public class Main {
-    private final static UserService userService = new UserServiceImpl();
-
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
-        userService.createUsersTable();
-
-        userService.saveUser("Frank", "Lampard", (byte) 47);
-        userService.saveUser("Paul", "Scholes", (byte) 25);
-        userService.saveUser("Steven", "Gerard", (byte) 56);
-        userService.saveUser("David", "Backham", (byte) 32);
-
-        userService.removeUserById(3);
-
-        userService.getAllUsers();
-
-        userService.cleanUsersTable();
-
-        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,33 +1,5 @@
 package jm.task.core.jdbc.util;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
 public class Util {
     // реализуйте настройку соеденения с БД
-    private static final String dbURL = "jdbc:mysql://localhost:3306/PP_1_1_3";
-    private static final String dbUserName = "root";
-    private static final String dbPassword = "vfq’cre’km2204";
-
-    public static Connection getConnection(){
-        Connection connection = null;
-
-        try {
-            connection = DriverManager.getConnection(dbURL, dbUserName, dbPassword);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            System.out.println("исключение в утил гет коннекшн");
-        }
-        return connection;
-    }
-
-    public static void closeConnection(Connection con) {
-        try {
-            con.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            System.out.println("исключение в утил клоуз коннекшн");
-        }
-    }
 }
